9c6ef75a09db16943534f66b545a417080999caf
shixiongfei <xiongfei.shi@icloud.com>
2023-11-09 23:09:58 +0800

    First version commit

---
+++ src/VegaViewer.tsx
@@ -0,0 +1,96 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import React from "react";
+import ReactDomServer from "react-dom/server";
+import vegaLite from "vega-lite";
+import gui from "gui";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const script = (filepath: string) =>
+  fs.readFileSync(path.join(dirname, filepath)).toString();
+
+const javascript = (filepath: string) =>
+  `<script type="text/javascript">${script(filepath)}</script>`;
+
+const javascripts = () => `
+${javascript("../vega/vega.min.js")}
+${javascript("../vega/vega-lite.min.js")}
+${javascript("../vega/vega-embed.min.js")}
+<script type="text/javascript">
+  function doEmbed(spec) {
+    vegaEmbed("#vis", spec, { actions: false }).then(function (result) {
+      var view = result.view;
+      var container = view.container();
+      view.addResizeListener(function (width, height) {
+        window.innerWidth = container.offsetWidth;
+        window.innerHeight = container.offsetHeight;
+        resize(container.offsetWidth, container.offsetHeight);
+      });
+      resize(container.offsetWidth, container.offsetHeight);
+    });
+  }
+</script>`;
+
+const VegaViewer = () => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Vega Viewer</title>
+      <style type="text/css">
+        {`body { margin: 0px; padding: 0px; } #vis { display: inline-block; }`}
+      </style>
+    </head>
+    <body>
+      <div id="vis"></div>
+      <div dangerouslySetInnerHTML={{ __html: javascripts() }}></div>
+    </body>
+  </html>
+);
+
+const showWindow = (vlSpec: vegaLite.TopLevelSpec) => {
+  const window = gui.Window.create({});
+  const contentview = gui.Container.create();
+  const browser = gui.Browser.create({
+    hardwareAcceleration: true,
+    webview2Support: true,
+  });
+
+  browser.addBinding("resize", (width: number, height: number) => {
+    window.setTitle(`${browser.getTitle()} #${process.pid}`);
+    window.setContentSize({ width, height });
+    window.center();
+  });
+
+  browser.onFinishNavigation = () => {
+    browser.executeJavaScript(
+      `doEmbed(${JSON.stringify(vlSpec)});`,
+      (result: boolean) => {
+        if (!result) {
+          console.log("Plot script execution failed, please try again!");
+          gui.MessageLoop.quit();
+        }
+      },
+    );
+  };
+
+  browser.setStyle({ flex: 1 });
+  browser.loadHTML(ReactDomServer.renderToString(<VegaViewer />), "");
+
+  contentview.setStyle({ flexDirection: "row" });
+  contentview.addChildView(browser);
+
+  window.onClose = () => gui.MessageLoop.quit();
+  window.setContentView(contentview);
+  window.setResizable(false);
+  window.activate();
+
+  if (!process.versions.yode) {
+    gui.MessageLoop.run();
+    process.exit(0);
+  }
+};
+
+export default { showWindow };
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { fileURLToPath } from "node:url";
+import { fork } from "node:child_process";
+import vega from "vega";
+import vegaLite from "vega-lite";
+import VegaViewer from "./VegaViewer.js";
+
+export const view = (spec: vega.Spec) =>
+  new vega.View(vega.parse(spec), { renderer: "none" });
+
+export const lite = (vlSpec: vegaLite.TopLevelSpec) => {
+  const spec = vegaLite.compile(vlSpec).spec;
+  return view(spec);
+};
+
+let nextAllowTime = new Date().getTime();
+
+const queuedExecute = (callback: () => void, interval = 1000) => {
+  const now = new Date().getTime();
+
+  if (now >= nextAllowTime) {
+    nextAllowTime += interval;
+    return callback();
+  }
+
+  setTimeout(callback, nextAllowTime - now);
+  nextAllowTime += interval;
+};
+
+if (process.argv[2] === "vega-viewer") {
+  process.once("message", VegaViewer.showWindow);
+}
+
+const filename = fileURLToPath(import.meta.url);
+
+export const plot = (vlSpec: vegaLite.TopLevelSpec) =>
+  queuedExecute(() => fork(filename, ["vega-viewer"]).send(vlSpec));
+
+export default { view, lite, plot };
